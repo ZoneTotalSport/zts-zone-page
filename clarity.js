0173de059241bee06ed010e201fa02d939d434b1
ZoneTotalSport <zts@hotmail.ca>
2026-04-19 14:01:10 -0400

    Analytics: ajout Microsoft Clarity (heatmaps + session replays)

---
+++ clarity.js
@@ -0,0 +1,9 @@
+/**
+ * Microsoft Clarity - heatmaps + session replays
+ * Project: zonetotalsport.ca (we8zk6ghu0)
+ */
+(function(c,l,a,r,i,t,y){
+  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "we8zk6ghu0");
